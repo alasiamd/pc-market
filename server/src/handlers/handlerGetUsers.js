518e48c4670d5a196fce7c97a95690b1cb13c4b1
alasiamd <alasiamd@gmail.com>
2023-10-18 22:45:17 -0300

    Se quitan comentarios que me servian para debuguear

--- server/src/handlers/handlerGetUsers.js
+++ server/src/handlers/handlerGetUsers.js
@@ -2,12 +2,9 @@ const { Users } = require("../db");
 
 const handlerGetUsers = async () => {
   try {
-    console.log("Manejador handlerGetUsers llamado");
     const data = await Users.findAll();
-    console.log("Datos recuperados:", data);
     return data;
   } catch (error) {
-    console.error("Error al recuperar usuarios:", error);
     throw error;
   }
 };
